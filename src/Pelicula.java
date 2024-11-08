80b514701c82037f3a01ec414f9c93292307227d
isaias ruben <ruben_4912@hotmail.com>
2024-11-07 22:16:25 -0300

    subiendo app buscador de peliculas starwars

---
+++ src/Pelicula.java
@@ -0,0 +1,7 @@
+public record Pelicula(String title,
+                       int episode_id,
+                       String opening_crawl,
+                       String director,
+                       String productor,
+                       String release_date) {
+}
